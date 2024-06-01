1a611b408dc7c2fb9f0c0f72d971a48deaffcc7a
Kusum Menaria <104812753+Menaria123@users.noreply.github.com>
2024-06-01 16:19:34 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState ,useEffect} from 'react'
+import navbar from './components/navbar'
+
+function App() {
+
+  return (
+    <>
+      <navbar/>
+      <div>
+      hello world!!!
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const navbar = () => {
+    return (
+        <nav>
+            <ul className="flex">
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+       </nav >
+    )
+}
+
+export default navbar
